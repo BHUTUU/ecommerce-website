87b912a45bdc810a83655c3b4b8c360060c96c25
BHUTUU <raj259942@gmail.com>
2023-07-29 14:48:10 +0530

    Errors fixed: database writing OK except image

--- ecommerce-website/src/main/java/com/bhutuu/ecomweb/ecommercewebsite/dao/AddProductDao.java
+++ ecommerce-website/src/main/java/com/bhutuu/ecomweb/ecommercewebsite/dao/AddProductDao.java
@@ -1,9 +1,9 @@
 package com.bhutuu.ecomweb.ecommercewebsite.dao;
 
+import com.bhutuu.ecomweb.ecommercewebsite.entity.AddProduct;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bhutuu.ecomweb.ecommercewebsite.entity.AddProduct;
 
 @Repository
 public interface AddProductDao extends CrudRepository<AddProduct, String> {
--- ecommerce-website/src/main/java/com/bhutuu/ecomweb/ecommercewebsite/service/AddProductService.java
+++ ecommerce-website/src/main/java/com/bhutuu/ecomweb/ecommercewebsite/service/AddProductService.java
@@ -1,19 +1,23 @@
 package com.bhutuu.ecomweb.ecommercewebsite.service;
+import com.bhutuu.ecomweb.ecommercewebsite.dao.AddProductDao;
+import com.bhutuu.ecomweb.ecommercewebsite.entity.AddProduct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.bhutuu.ecomweb.ecommercewebsite.entity.AddProduct;
-import com.bhutuu.ecomweb.ecommercewebsite.dao.AddProductDao;
+
+
 
 @Service
 public class AddProductService {
-    private final AddProductDao addProductDao;
-
     @Autowired
-    public AddProductService(AddProductDao addProductDao) {
-        this.addProductDao = addProductDao;
-    }
+    private  AddProductDao addProductDao;
 
-    public void saveProduct(AddProduct product) {
-        addProductDao.save(product);
+    // @Autowired
+    // public AddProductService(AddProductDao addProductDao) {
+    //     this.addProductDao = addProductDao;
+    // }
+
+    public AddProduct saveProduct(AddProduct product) {
+
+        return addProductDao.save(product);
     }
 }
